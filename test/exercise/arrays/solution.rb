2fbd299bfa2a96d9fb17931f8fa1a7dc73dc85ed
Max Erastov <max.erastov@gmail.com>
2021-04-10 19:29:12 +0300

    Complete method replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,15 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_elem = array[0]
+        array.each do |elem|
+          if elem > max_elem
+            max_elem = elem
+          else
+            max_elem
+          end
+        end
+        array.map { |elem| elem.positive? ? max_elem : elem }
       end
 
       def search(_array, _query)
