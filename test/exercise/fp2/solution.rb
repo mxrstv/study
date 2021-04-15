f7fe9aa96a62b550e098a0f7d2c633eff6787dab
Max Erastov <max.erastov@gmail.com>
2021-04-15 23:56:06 +0300

    Add my_each

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,7 +5,19 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&block)
+        
+        if self[1..].empty?
+          yield self.first
+          p "last #{self}"
+        else
+          yield self.first
+          p "iter #{self}"
+          self[1..].my_each(&block)
+        end
+        
+        self
+      end
 
       # Написать свою функцию my_map
       def my_map; end
